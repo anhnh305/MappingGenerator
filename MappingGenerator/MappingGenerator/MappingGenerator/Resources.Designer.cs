f2d8b84e570cb6f6f4d0162d7ff598da9109cac4
unknown <nguyenhaianh98bn@gmail.com>
2020-12-08 20:21:07 +0700

    HA first

--- MappingGenerator/MappingGenerator/MappingGenerator/Resources.Designer.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/Resources.Designer.cs
@@ -6,6 +6,7 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
+// This is the single line comment
 //------------------------------------------------------------------------------
 
 namespace MappingGenerator
