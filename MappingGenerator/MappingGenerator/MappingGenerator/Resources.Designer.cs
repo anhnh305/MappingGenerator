3ed28f9239259bab21c7e2e59abc49180df4ba3b
unknown <nguyenhaianh98bn@gmail.com>
2020-12-08 20:23:25 +0700

    HA tenth aka final

--- MappingGenerator/MappingGenerator/MappingGenerator/Resources.Designer.cs
+++ MappingGenerator/MappingGenerator/MappingGenerator/Resources.Designer.cs
@@ -6,7 +6,7 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
-// Ninth comment
+// Final comment
 //------------------------------------------------------------------------------
 
 namespace MappingGenerator
